a87d58aa3246a7c9164c68218bbc108f8ba630ba
imandeaM <maryimande@gmail.com>
2023-02-20 20:56:42 -0800

    do hard

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations
+ * main- numbers
  *
- * Return: Always 0 (Success)
+ * Return- 0 succeful
  */
-
 int main(void)
-
 {
 int i, j;
 for (i = 0; i < 100; i++)
 {
-for (j = 0; j < 100; j++)
+for (j = i; j < 100; j++)
 {
-if (i < j)
-{
-
-	putchar((i / 10) + 48);
-	putchar((i % 10) + 48);
-	putchar(' ');
-	putchar((j / 10) + 48);
-	putchar((j % 10) + 48);
-
-if (i != 98 || j != 9)
-{
-	putchar(',');
-	putchar(' ');
+int tens1 = i / 10;
+int ones1 = i % 10;
+int tens2 = j / 10;
+int ones2 = j % 10;
+putchar('0' + tens1);
+putchar('0' + ones1);
+putchar(' ');
+putchar('0' + tens2);
+putchar('0' + ones2);
+putchar(',');
+putchar(' ');
 }
 }
-}
-}
-	putchar('\n');
 return (0);
 }
