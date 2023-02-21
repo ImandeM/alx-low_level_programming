ff3e5a7658f9bc97dffcadff18d3a1c2da4f92bd
imandeaM <maryimande@gmail.com>
2023-02-20 20:40:00 -0800

    hard things

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,19 +16,21 @@ for (j = 0; j < 100; j++)
 {
 if (i < j)
 {
-putchar((i / 10) + 48);
-putchar((i % 10) + 48);
-putchar(' ');
-putchar((j / 10) + 48);
-putchar((j % 10) + 48);
-if (i != 98 || j != 9);
+
+	putchar((i / 10) + 48);
+	putchar((i % 10) + 48);
+	putchar(' ');
+	putchar((j / 10) + 48);
+	putchar((j % 10) + 48);
+
+if (i != 98 || j != 9)
 {
-putchar(',');
-putchar(' ');
+	putchar(',');
+	putchar(' ');
 }
 }
 }
 }
-putchar('\n');
+	putchar('\n');
 return (0);
 }
