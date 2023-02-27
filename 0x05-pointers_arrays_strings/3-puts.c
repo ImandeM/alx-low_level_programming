c5d115a0ab03ef5b583eb2b940c1d6827821b4c6
imandeaM <maryimande@gmail.com>
2023-02-27 10:45:05 -0800

    put.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,40 +1,19 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer.
- * @s: The pointer to convert
+ * _puts - prints a string
+ * @str: the string
  *
- * Return: A integer
+ * Return: the length of the string
  */
-int _atoi(char *s)
+void _puts(char *str)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int i = 0;
 
-	while (s[c])
+	while (str[i] != '\0')
 	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
-
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-
-		if (isi == 1)
-		{
-			break;
-		}
-
-		c++;
+		_putchar(str[i]);
+		i++;
 	}
-
-	ni *= min;
-	return (ni);
+	_putchar('\n');
 }
